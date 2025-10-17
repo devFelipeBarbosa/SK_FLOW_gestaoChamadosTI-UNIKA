a5e03021f1de77559e410f5c7aba00640a2598c2
Felipe C. Barbosa <cavalcantebarbosa@gmail.com>
2025-10-17 20:49:39 -0300

    Atualização do commit message para refletir as mudanças feitas no código.

--- eventos/registroSolicitacao/registroSolicitacao.js
+++ eventos/registroSolicitacao/registroSolicitacao.js
@@ -1,104 +1,107 @@
-// Registro da Solicitação - na tabela AD_CHAMADOSTI (INICIALIZACÃO) 
+// Registro da Solicitação - na tabela AD_CHAMADOTI (INICIALIZACÃO) 
 var solicitacao = getIdInstanceProcesso(); // ID da Solicitação 
 var codUsu = getUsuarioInclusao(); // ID do Usuário Solicitante 
 var tabChamado = getLinhasFormulario("AD_CHAMADOTI"); // Retorna as linhas Tabela de Chamados TI 
 var linhaExistente = false; // Boleano para verificar existência de chamado TI para a solicitação 
+var c = null; // contador genérico para encontrar linha cadastro do Formulário
 
 // Campos do Formulário Embarcado 
-var tipo = getCampo("TIPO") || 'I'; 
-var codLocal = getCampo("CODLOCAL") || Number(0); 
-var categoria = getCampo("CATEGORIA") || Number(0); 
-var urgencia = getCampo("URGENCIA") || Number(0); 
-var titulo = getCampo("TITULO") || ''; 
-var descricao = getCampo("TEXTOCHAMADO") || ''; 
-var anexo1 = getCampo("ANEXO1"); 
-var anexo2 = getCampo("ANEXO2"); 
-var anexo3 = getCampo("ANEXO3"); 
+var tipo = getCampo("TIPO") || 'I';
+var codLocal = getCampo("CODLOCAL") || Number(0);
+var categoria = getCampo("CATEGORIA") || Number(0);
+var urgencia = getCampo("URGENCIA") || Number(0);
+var titulo = getCampo("TITULO") || '';
+var descricao = getCampo("TEXTOCHAMADO") || '';
+var anexo1 = getCampo("ANEXO1");
+var anexo2 = getCampo("ANEXO2");
+var anexo3 = getCampo("ANEXO3");
 
 // Verifica se já existe uma linha na tabela de chamados TI para a solicitação atual 
-for (var i = 0; i < tabChamado.length; i++) { 
-    if (tabChamado[i].getCampo("IDINSTPRN") == solicitacao) { 
-        linhaExistente = true; 
-        break; 
-    } 
+for (var i = 0; i < tabChamado.length; i++) {
+    if (tabChamado[i].getCampo("IDINSTPRN") == solicitacao) {
+        linhaExistente = true;
+        c = i;
+        break;
+    }
+}
 
-    if (!linhaExistente) { 
+if (!linhaExistente) {
+    var novaLinhaTabChamado = novaLinhaFormulario("AD_CHAMADOTI");
+    novaLinhaTabChamado.setCampo("CODUSU", codUsu);
+    novaLinhaTabChamado.setCampo("TIPO", tipo);
+    novaLinhaTabChamado.setCampo("ID_CATEGORIATI", categoria);
+    novaLinhaTabChamado.setCampo("CODLOCAL", codLocal);
+    novaLinhaTabChamado.setCampo("TITULO", titulo);
+    novaLinhaTabChamado.setCampo("TEXTOCHAMADO", descricao);
+    novaLinhaTabChamado.setCampo("STATUSCHAMADO", 'N');
+    novaLinhaTabChamado.setCampo("ORIGEMCHAMADO", 'F');
+    novaLinhaTabChamado.setCampo("URGENCIA", urgencia);
+    novaLinhaTabChamado.setCampo("IMPACTO", urgencia); // regra de negócio inicial: Impacto igual Urgência 
+    novaLinhaTabChamado.setCampo("PRIORIDADE", 'M');
 
-        var novaLinhaTabChamado = novaLinhaFormulario("AD_CHAMADOTI"); 
-        novaLinhaTabChamado.setCampo("CODUSU", codUsu); 
-        novaLinhaTabChamado.setCampo("TIPO", tipo); 
-        novaLinhaTabChamado.setCampo("ID_CATEGORIATI", categoria); 
-        novaLinhaTabChamado.setCampo("CODLOCAL", codLocal); 
-        novaLinhaTabChamado.setCampo("TITULO", titulo); 
-        novaLinhaTabChamado.setCampo("TEXTOCHAMADO", descricao); 
-        novaLinhaTabChamado.setCampo("STATUSCHAMADO", 'N'); 
-        novaLinhaTabChamado.setCampo("ORIGEMCHAMADO", 'F'); 
-        novaLinhaTabChamado.setCampo("URGENCIA", urgencia); 
-        novaLinhaTabChamado.setCampo("IMPACTO", urgencia); // regra de negócio inicial: Impacto igual Urgência 
-        novaLinhaTabChamado.setCampo("PRIORIDADE", 'M'); 
-         
-        if (anexo1) { 
-            novaLinhaTabChamado.setCampo("ANEXO1", anexo1); 
-        } 
-        if (anexo2) { 
-            novaLinhaTabChamado.setCampo("ANEXO2", anexo2); 
-        } 
-        if (anexo3) { 
-            novaLinhaTabChamado.setCampo("ANEXO3", anexo3); 
-        } 
-        novaLinhaTabChamado.setCampo("CICLO1", 'NA'); // regra de negócio inicial: CICLO1 inicial igual a NA 
-        novaLinhaTabChamado.setCampo("CODUSUALTER", codUsu); 
-        novaLinhaTabChamado.setCampo("TERCEIRO", 'NA'); // TERCEIRO regra do negócio: igual a NA no inicio 
-        novaLinhaTabChamado.setCampo("STATUSTERCEIRO", 'NA'); // STATUSTERCEIRO regra do negócio: igual a NA no inicio 
-        novaLinhaTabChamado.setCampo("CICLO2", 'N'); // CICLO2 regra do negócio: igual a N no inicio 
-        novaLinhaTabChamado.setCampo("CICLO3", 'NA'); // CICLO3 regra do negócio: igual a NA no inicio CLICO3 
-        novaLinhaTabChamado.setCampo("AVALIACAO", '0'); // AVALIACAO regra do negócio: igual a 0 (texto) no inicio 
+    if (anexo1) {
+        novaLinhaTabChamado.setCampo("ANEXO1", anexo1);
+    }
+    if (anexo2) {
+        novaLinhaTabChamado.setCampo("ANEXO2", anexo2);
+    }
+    if (anexo3) {
+        novaLinhaTabChamado.setCampo("ANEXO3", anexo3);
+    }
 
-        try { 
-            novaLinhaTabChamado.save(); 
-        } catch (e) { 
-            console.error("Erro ao salvar os campos alterados: ", e); 
-            throw new Error("Erro ao <b>salvar os campos alterados</b>! <br>" + e.message); 
-        } 
+    novaLinhaTabChamado.setCampo("CICLO1", 'NA'); // regra de negócio inicial: CICLO1 inicial igual a NA 
+    novaLinhaTabChamado.setCampo("CODUSUALTER", codUsu);
+    novaLinhaTabChamado.setCampo("TERCEIRO", 'NA'); // TERCEIRO regra do negócio: igual a NA no inicio 
+    novaLinhaTabChamado.setCampo("STATUSTERCEIRO", 'NA'); // STATUSTERCEIRO regra do negócio: igual a NA no inicio 
+    novaLinhaTabChamado.setCampo("CICLO2", 'N'); // CICLO2 regra do negócio: igual a N no inicio 
+    novaLinhaTabChamado.setCampo("CICLO3", 'NA'); // CICLO3 regra do negócio: igual a NA no inicio CLICO3 
+    novaLinhaTabChamado.setCampo("AVALIACAO", '0'); // AVALIACAO regra do negócio: igual a 0 (texto) no inicio 
 
-    } else { 
+    try {
+        novaLinhaTabChamado.save();
+    } catch (e) {
+        console.error("Erro ao CRIAR o chamado TI: ", e);
+        throw new Error("Erro ao <b>CRIAR o chamado TI</b>! <br>" + e.message);
+    }
 
-        tabChamado[i].setCampo("CODUSU", codUsu); 
-        tabChamado[i].setCampo("CODUSU", codUsu); 
-        tabChamado[i].setCampo("TIPO", tipo); 
-        tabChamado[i].setCampo("ID_CATEGORIATI", categoria); 
-        tabChamado[i].setCampo("CODLOCAL", codLocal); 
-        tabChamado[i].setCampo("TITULO", titulo); 
-        tabChamado[i].setCampo("TEXTOCHAMADO", descricao); 
-        tabChamado[i].setCampo("STATUSCHAMADO", 'N'); 
-        tabChamado[i].setCampo("ORIGEMCHAMADO", 'F'); 
-        tabChamado[i].setCampo("URGENCIA", urgencia); 
-        tabChamado[i].setCampo("IMPACTO", urgencia); // regra de negócio inicial: Impacto igual Urgência 
-        tabChamado[i].setCampo("PRIORIDADE", 'M'); 
+} else {
 
-        if (anexo1) { 
-            tabChamado[i].setCampo("ANEXO1", anexo1); 
-        } 
-        if (anexo2) { 
-            tabChamado[i].setCampo("ANEXO2", anexo2); 
-        } 
-        if (anexo3) { 
-            tabChamado[i].setCampo("ANEXO3", anexo3); 
-        } 
+    tabChamado[c].setCampo("CODUSU", codUsu);
+    tabChamado[c].setCampo("TIPO", tipo);
+    tabChamado[c].setCampo("ID_CATEGORIATI", categoria);
+    tabChamado[c].setCampo("CODLOCAL", codLocal);
+    tabChamado[c].setCampo("TITULO", titulo);
+    tabChamado[c].setCampo("TEXTOCHAMADO", descricao);
+    tabChamado[c].setCampo("STATUSCHAMADO", 'N');
+    tabChamado[c].setCampo("ORIGEMCHAMADO", 'F');
+    tabChamado[c].setCampo("URGENCIA", urgencia);
+    tabChamado[c].setCampo("IMPACTO", urgencia); // regra de negócio inicial: Impacto igual Urgência 
+    tabChamado[c].setCampo("PRIORIDADE", 'M');
 
-        tabChamado[i].setCampo("CICLO1", 'NA'); // regra de negócio inicial: CICLO1 inicial igual a NA 
-        tabChamado[i].setCampo("CODUSUALTER", codUsu); 
-        tabChamado[i].setCampo("TERCEIRO", 'NA'); // TERCEIRO regra do negócio: igual a NA no inicio 
-        tabChamado[i].setCampo("STATUSTERCEIRO", 'NA'); // STATUSTERCEIRO regra do negócio: igual a NA no inicio 
-        tabChamado[i].setCampo("CICLO2", 'N'); // CICLO2 regra do negócio: igual a N no inicio 
-        tabChamado[i].setCampo("CICLO3", 'NA'); // CICLO3 regra do negócio: igual a NA no inicio CLICO3 
-        tabChamado[i].setCampo("AVALIACAO", '0'); // AVALIACAO regra do negócio: igual a 0 (texto) no inicio     
+    if (anexo1) {
+        tabChamado[c].setCampo("ANEXO1", anexo1);
+    }
+    if (anexo2) {
+        tabChamado[c].setCampo("ANEXO2", anexo2);
+    }
+    if (anexo3) {
+        tabChamado[c].setCampo("ANEXO3", anexo3);
+    }
 
-        try { 
-            salvarCamposAlterados(); 
-        } catch (e) { 
-            console.error("Erro ao salvar os campos alterados: ", e); 
-            throw new Error("Erro ao <b>salvar os campos alterados</b>! <br>" + e.message); 
-        } 
-    } 
+    tabChamado[c].setCampo("CICLO1", 'NA'); // regra de negócio inicial: CICLO1 inicial igual a NA 
+    tabChamado[c].setCampo("CODUSUALTER", codUsu);
+    tabChamado[c].setCampo("TERCEIRO", 'NA'); // TERCEIRO regra do negócio: igual a NA no inicio 
+    tabChamado[c].setCampo("STATUSTERCEIRO", 'NA'); // STATUSTERCEIRO regra do negócio: igual a NA no inicio 
+    tabChamado[c].setCampo("CICLO2", 'N'); // CICLO2 regra do negócio: igual a N no inicio 
+    tabChamado[c].setCampo("CICLO3", 'NA'); // CICLO3 regra do negócio: igual a NA no inicio CLICO3 
+    tabChamado[c].setCampo("AVALIACAO", 0); // AVALIACAO regra do negócio: igual a 0 (texto) no inicio     
+    
+    try {
+        salvarCamposAlterados();
+    } catch (e) {
+        console.error("Erro ao registrar a solicitação na tabela de chamados TI: ", e);
+        throw new Error("Erro ao <b>registrar a solicitação na tabela de chamados TI</b>! <br>" + e.message);
+
+    }
+
 }
