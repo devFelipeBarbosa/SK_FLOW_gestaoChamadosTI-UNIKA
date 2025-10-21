3a5e0a5f740a1512794193810ff54ed7872235b4
Felipe C. Barbosa <cavalcantebarbosa@gmail.com>
2025-10-21 18:27:52 -0300

    Versão 87 - Iniciando a Eventos Atividade - Em andamento

---
+++ eventos/emAtendimento/emAtendimentoCriacao.js
@@ -0,0 +1,30 @@
+// Eem Atendimento, Criação
+var solicitacao = getIdInstanceProcesso(); // ID da Solicitação 
+var codUsu = getUsuarioInclusao(); // ID do Usuário Solicitante 
+var tabChamado = getLinhasFormulario("AD_CHAMADOTI"); // Retorna as linhas Tabela de Chamados TI (AD_CHAMADOTI)
+var linhaExistente = false; // Boleano para verificar existência de chamado TI para a solicitação 
+var c = null; // contador genérico para encontrar linha cadastro do Formulário
+
+// Verifica se já existe uma linha na tabela de chamados TI para a solicitação atual 
+for (var i = 0; i < tabChamado.length; i++) {
+    if (tabChamado[i].getCampo("IDINSTPRN") == solicitacao) {
+        linhaExistente = true;
+        c = i;
+        break;
+    }
+}
+
+if (linhaExistente) {
+    tabChamado[c].setCampo("CODUSUALTER", codUsu);
+    tabChamado[c].setCampo("IDTAREFA", 'UserTask_1dsyzbu');
+
+    tabChamado[c].setCampo("STATUSCHAMADO", 'E'); // Atualiza o status do chamado TI para 'A' - Novo quando desatribuído a tarefa
+
+    try {
+        salvarCamposAlterados();
+    } catch (e) {
+        console.error("Erro ao atualizar a solicitação na tabela de chamados TI: ", e);
+        throw new Error("Erro ao <b>atualizar a solicitação na tabela de chamados TI</b>! <br>" + e.message);
+
+    }
+}
--- eventos/tratativaEmailTriagem/tratativaEmail.js
+++ eventos/tratativaEmailTriagem/tratativaEmail.js
@@ -29,17 +29,29 @@ var categoria = tabChamado[c].getCampo("ID_CATEGORIATI");
 var nomeCategoria = buscarDado("DESCRICAO", "AD_CATEGORIATI", "ID_CATEGORIATI = :ID_CATEGORIATI", [categoria]) || 'Não informada';
 var titulo = tabChamado[c].getCampo("TITULO") || '';
 var descricao = tabChamado[c].getCampo("TEXTOCHAMADO") || '';
-var dataPrevista = tabChamado[c].getCampo("PREVATENDIMENTO") || 'Sem data prevista';
+var dataPrevista = tabChamado[c].getCampo("PREVATENDIMENTO");
+var dataFormatada;
 
-var textoEmail = "<b>Titulo: </b>" + titulo + ";<br>" + "<b>Descrição: </b><br>" + descricao;
+if (dataPrevista) {
+    dataPrevista = new Date(dataPrevista);
+    var dia = dataPrevista.getDate();
+    var mes = dataPrevista.getMonth() + 1;
+    var ano = dataPrevista.getFullYear();
+    dataFormatada = dia + "/" + mes + "/" + ano;
+} else {
+    dataFormatada = 'Não informada';
+}
 
-textoEmail += "<br>-------------- Dados do Chamado ---------------<br>"
+
+var textoEmail = "<b>Titulo: </b>" + titulo + ";<br>" + "<b>Descrição: </b>" + descricao;
+
+textoEmail += "<br><br>-------------- Dados do Chamado ---------------<br>"
     + "<b>Solitação: </b>" + solicitacao + ";<br>"
     + "<b>Usuário: </b>" + codUsu + " - " + nome + ";<br>"
     + "<b>Tipo: </b>" + tipo + ";<br>"
     + "<b>Localização: </b>" + codLocal + " - " + nomeLocal + ";<br>"
     + "<b>Categoria: </b>" + categoria + " - " + nomeCategoria + ";<br>"
-    + "<b>Data Prevista de Entrega: </b>" + dataPrevista + "(OBS.: a previsão de entrega pode sofrer alterações ao longo do atendimento).<br>";
+    + "<b>Data Prevista de Entrega: </b>" + dataFormatada + " (OBS.: a previsão de entrega pode sofrer alterações ao longo do atendimento).<br>";
 
 setCampo("CORPOEMAIL", textoEmail);
 
@@ -50,5 +62,3 @@ try {
     console.error("Erro no Evento de Ação da Triagem: ", e);
     throw new Error("<b>Falha na Validação/Ação da Triagem:</b> <br>" + e.message);
 }
-
-
